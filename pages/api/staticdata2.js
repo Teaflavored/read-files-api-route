af7d205bdb65412b4a3777bd9013b4b850bde322
teaflavored <auster.s.chen@gmail.com>
2023-05-18 18:03:28 -0700

    test

---
+++ pages/api/staticdata2.js
@@ -0,0 +1,18 @@
+import path from "path";
+import { promises as fs } from "fs";
+
+export default async function handler(req, res) {
+  const { filename } = req.query;
+
+  //Find the absolute path of the json directory
+  const jsonDirectory = path.join(process.cwd(), "json");
+  //Read the json data file data.json
+  const fileContents = await fs.readFile(jsonDirectory + "/data.json", "utf8");
+  const fileContents2 = await fs.readFile(
+    jsonDirectory + `/${filename}.json`,
+    "utf8"
+  );
+
+  //Return the content of the data file in json format
+  res.status(200).json([fileContents, fileContents2]);
+}
